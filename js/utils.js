76f6e520b6136ea6348024b63458cc85027502de
greustal <308625201@qq.com>
2023-08-03 09:54:58 +0800

    Site updated: 2023-08-03 09:54:58

--- js/utils.js
+++ js/utils.js
@@ -439,15 +439,15 @@ var anzhiyu = {
       if (monthCount >= 1) {
         result = datePost.toLocaleDateString().replace(/\//g, "-");
       } else if (dayCount >= 1 && dayCount <= 3) {
-        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.date_suffix.day;
+        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.dateSuffix.day;
       } else if (dayCount > 3) {
         result = datePost.getMonth() + 1 + "/" + datePost.getDate();
       } else if (hourCount >= 1) {
-        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.date_suffix.hour;
+        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.dateSuffix.hour;
       } else if (minuteCount >= 1) {
-        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.date_suffix.min;
+        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.dateSuffix.min;
       } else {
-        result = GLOBAL_CONFIG.date_suffix.just;
+        result = GLOBAL_CONFIG.dateSuffix.just;
       }
     } else {
       result = parseInt(dateDiff / day);
