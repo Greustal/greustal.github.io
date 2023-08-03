d29a088dc5f073b2232b7826a9ed68b4657a57b1
greustal <308625201@qq.com>
2023-08-03 09:44:45 +0800

    Site updated: 2023-08-03 09:44:45

--- js/utils.js
+++ js/utils.js
@@ -422,13 +422,14 @@ var anzhiyu = {
       if (monthCount >= 1) {
         result = datePost.toLocaleDateString().replace(/\//g, "-");
       } else if (dayCount >= 1) {
-        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.date_suffix.day;
+        console.log(GLOBAL_CONFIG)
+        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.dateSuffix.day;
       } else if (hourCount >= 1) {
-        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.date_suffix.hour;
+        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.dateSuffix.hour;
       } else if (minuteCount >= 1) {
-        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.date_suffix.min;
+        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.dateSuffix.min;
       } else {
-        result = GLOBAL_CONFIG.date_suffix.just;
+        result = GLOBAL_CONFIG.dateSuffix.just;
       }
     } else if (simple) {
       const monthCount = dateDiff / month;
